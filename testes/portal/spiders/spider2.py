9da6a1527c0e1ca4f5f69366a3c2cbb638725bc2
Vinícius Pessoa <vini.pessoa7@gmail.com>
2019-10-02 10:39:56 -0400

    spider2 download csv

--- testes/portal/spiders/spider2.py
+++ testes/portal/spiders/spider2.py
@@ -10,10 +10,12 @@ import time
 # Preferencias
 profile = webdriver.FirefoxProfile()
 profile.set_preference('browser.download.folderList', 2) # custom location
+profile.set_preference('browser.download.panel.shown', False)
 profile.set_preference('browser.download.manager.showWhenStarting', False)
-profile.set_preference('browser.download.dir', '/home/vinicius/git/PAIC/csv/')
-profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'application/csv')
-profile.set_preference("browser.download.panel.shown", False)
+profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'application/csv,text/csv,application/pdf,application/x-pdf')
+profile.set_preference('browser.helperApps.neverAsk.openFile', 'application/csv,text/csv,application/pdf,application/x-pdf')
+profile.set_preference('browser.download.dir', '/home/vini/git/PAIC/csv/')
+profile.set_preference("pdfjs.disabled", 'true')
 
 # Acesso do Driver
 b = webdriver.Firefox(profile)
@@ -21,16 +23,15 @@ b.get('http://www.transparencia.am.gov.br/pessoal/')
 
 select_orgao = b.find_element_by_id("nav_orgaos")
 select_orgao.click()
-orgaos = select_orgao.text.split("\n");
+orgaos = select_orgao.text.split("\n")
 
 for orgao in orgaos:
     select_ano = b.find_element_by_id("nav_year")
     select_ano.click()
-    anos = select_ano.text.split("\n");
+    anos = select_ano.text.split("\n")
     for ano in anos:
 
 
-        print(orgao, ano);
         se_orgao = Select(b.find_element_by_id("nav_orgaos"))
         se_ano = Select(b.find_element_by_id("nav_year"))
 
@@ -38,16 +39,19 @@ for orgao in orgaos:
         se_ano.select_by_visible_text(ano)
         time.sleep(1)
 
-        tabela = b.find_element_by_xpath("//table[@class='a-table']/tbody")
-        mes_txt = tabela.find_elements_by_xpath("/td/td")
-        for i in mes_txt:
-            print(i)
-
-        try:
-            btn_csv = b.find_element_by_xpath("//a[contains(.,'.csv')]")
-            btn_csv.click()
-        except:     
-            btn_pdf = b.find_element_by_xpath("//a[contains(.,'.pdf')]")
-            btn_pdf.click()
+        meses_tr = b.find_elements_by_xpath(".//*[@class='a-table']/tbody/tr")
+        for i in range(2, len(meses_tr)):
+            mes_txt = b.find_element_by_xpath(".//*[@class='a-table']/tbody/tr["+str(i)+"]/td[1]").text
+            print(orgao + "_" + ano + "_", end='')
+            print(mes_txt, end='')
+            try:    
+                btn_csv = b.find_element_by_xpath(".//*[@class='a-table']/tbody/tr["+str(i)+"]/td[2]/a[2]")
+                print(btn_csv.text)
+                btn_csv.click()
+            except:     
+                btn_pdf = b.find_element_by_xpath(".//*[@class='a-table']/tbody/tr["+str(i)+"]/td[2]/a[1]")
+                print(btn_pdf.text)
+                btn_pdf.click()
 
     b.quit()
+    break
